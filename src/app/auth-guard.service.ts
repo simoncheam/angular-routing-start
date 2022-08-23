0daf0b8d1758ec61a48c12cac293c9c43f29458d
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-23 10:42:34 -0400

    server child routes protected

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   Router,
   RouterStateSnapshot,
+  UrlTree,
 } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -22,5 +24,12 @@ export class AuthGuard implements CanActivate {
         this.router.navigate(["/"]); // controls access
       }
     });
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
   }
 }
