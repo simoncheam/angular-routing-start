d226d15914e9dc27ffd8e3aa7c519c156612d024
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-23 10:14:38 -0400

    protecting nested child routes with canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@
 
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./auth-guard.service";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
@@ -20,6 +21,7 @@ const appRoutes: Routes = [
   },
   {
     path: "servers",
+    canActivate: [AuthGuard],
     component: ServersComponent,
     children: [
       { path: ":id", component: ServerComponent },
